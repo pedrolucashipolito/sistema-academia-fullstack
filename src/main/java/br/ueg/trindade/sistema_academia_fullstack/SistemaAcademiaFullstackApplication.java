c03a003214454c7aaf317eff5813ef910a80d777
pedrolucashipolito <pedrolucashdeoliveirapes@gmail.com>
2026-09-12 18:15:07 -0300

    Commit inicial

---
+++ src/main/java/br/ueg/trindade/sistema_academia_fullstack/SistemaAcademiaFullstackApplication.java
@@ -0,0 +1,13 @@
+package br.ueg.trindade.sistema_academia_fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaAcademiaFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaAcademiaFullstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ueg/trindade/sistema_academia_fullstack/SistemaAcademiaFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ueg.trindade.sistema_academia_fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAcademiaFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
