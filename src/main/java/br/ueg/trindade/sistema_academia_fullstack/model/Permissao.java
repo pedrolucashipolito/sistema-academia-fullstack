e05f0412f6dee52c84871f578078dfcf9863e80f
pedrolucashipolito <pedrolucashdeoliveirapes@gmail.com>
2026-09-17 08:09:24 -0300

    Adicionando classes  permissões e aparelhos

---
+++ src/main/java/br/ueg/trindade/sistema_academia_fullstack/model/Permissao.java
@@ -0,0 +1,51 @@
+package br.ueg.trindade.sistema_academia_fullstack.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Permissao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    private String descricao;
+
+    public Permissao() {
+    }
+
+    public Permissao(Long id, String nome, String descricao) {
+        this.id = id;
+        this.nome = nome;
+        this.descricao = descricao;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
---
+++ src/main/java/br/ueg/trindade/sistema_academia_fullstack/repository/AparelhoRepository.java
@@ -0,0 +1,8 @@
+package br.ueg.trindade.sistema_academia_fullstack.repository;
+
+import br.ueg.trindade.sistema_academia_fullstack.model.Aparelho;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AparelhoRepository extends JpaRepository<Aparelho, Long> {
+
+}
